43c4d49ddb3c368f62c0d6433cab343717f910f9
Christian Helmich <kagekirin@gmail.com>
2012-03-01 12:11:56 +0900

    modified regen.sh to update octopress source before generating site

--- regen.sh
+++ regen.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-echo "regenerating site"
+echo "saving site changes"
 git add _config.yml
 git commit -m "updated _config.yml"
 git add sass
 git commit -m "updated stylesheets"
 git add source
 git commit -m "updated blog pages"
+git commit -am "saved other changes"
+
+echo "updating octopress"
+git pull octopress master
+
+echo "regenerating site"
 rake generate
 rake deploy
+
+echo "deploying site"
 git add . 
 git commit -am "regenerated blog"
 git push origin source
