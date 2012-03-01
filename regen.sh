2e1ecf79a88266bc5f93ab6297c3a304096e15fd
Christian Helmich <kagekirin@gmail.com>
2012-03-01 17:18:55 +0900

    updated regen.sh with remote syncing

--- regen.sh
+++ regen.sh
@@ -9,6 +9,9 @@ git add source
 git commit -m "updated blog pages"
 git commit -am "saved other changes"
 
+echo "syncing with remote origin"
+git pull origin source
+
 echo "updating octopress"
 git pull octopress master
 
